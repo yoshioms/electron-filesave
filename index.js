688716d67d5004cc8dabce08bbc269a8b1982945
Yoshihisa Ozaki <yoozaki@tohmatsu.co.jp>
2016-12-31 14:28:37 +0900

    Save dialog

---
+++ index.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow, dialog } = require('electron');
+const ipc = require('electron').ipcMain
+
+ipc.on('save-dialog', function(event) {
+    const options = {
+        title: 'Save an Image',
+        filters: [
+            { name: 'Images', extensions: ['jpg', 'png', 'gif'] }
+        ]
+    }
+    dialog.showSaveDialog(options, function(filename) {
+        event.sender.send('saved-file', filename)
+    })
+})
+
+var mainWindows = null;
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({ width: 400, height: 100 });
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
